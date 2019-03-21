afb2737f1298893df1223ca17e292efe43fe061e
Alex Putkov <alex.putkov@thomsonreuters.com>
2019-03-20 21:03:51 -0400

    Update Estimates3.py

--- Estimates3.py
+++ Estimates3.py
@@ -7,30 +7,34 @@ dfFY0, err = ek.get_data("IBM",
                   'TR.EPSActValue(Period=FY0, Curn=USD)'])
  
 dfFY1, err = ek.get_data("IBM", 
-                 ['TR.RevenueMeanEstimate(Period=FY1, Scale=6, Curn=USD, Precision=2)',
-                  'TR.RevenueHigh(Period=FY1, Scale=6, Curn=USD)', 'TR.RevenueLow(Period=FY1, Scale=6, Curn=USD)',
-                  'TR.EBITDAMean(Period=FY1, Scale=6, Curn=USD, Precision=2)',
-                  'TR.EBITDAHigh(Period=FY1, Scale=6, Curn=USD)', 'TR.EBITDALow(Period=FY1, Scale=6, Curn=USD)',
+                 ['TR.RevenueMeanEstimate(Period=FY1, Scale=6, Curn=USD, Precision=0)',
+                  'TR.RevenueHigh(Period=FY1, Scale=6, Curn=USD, Precision=0)',
+                  'TR.RevenueLow(Period=FY1, Scale=6, Curn=USD, Precision=0)',
+                  'TR.EBITDAMean(Period=FY1, Scale=6, Curn=USD, Precision=0)',
+                  'TR.EBITDAHigh(Period=FY1, Scale=6, Curn=USD, Precision=0)',
+                  'TR.EBITDALow(Period=FY1, Scale=6, Curn=USD, Precision=0)',
                   'TR.EPSMeanEstimate(Period=FY1, Curn=USD)', 'TR.EPSHigh(Period=FY1, Curn=USD)', 'TR.EPSLow(Period=FY1, Curn=USD)'])
 
 # there can be only one request to get the field in get_data call
 # for the same field but with different parameters, we use another call                   
 dfFY1Roll, err = ek.get_data("IBM", 
-                     ['TR.RevenueMeanEstimate(Period=FY1, Scale=6, Curn=USD, RollPeriods=False)',
-                      'TR.EBITDAMean(Period=FY1, Scale=6, Curn=USD, RollPeriods=False)',
-                      'TR.EPSMeanEstimate(Period=FY1, Curn=USD, RollPeriods=False, Precision=2)'])
+                     ['TR.RevenueMeanEstimate(Period=FY1, Scale=6, Curn=USD, RollPeriods=False, Precision=0)',
+                      'TR.EBITDAMean(Period=FY1, Scale=6, Curn=USD, RollPeriods=False, Precision=0)',
+                      'TR.EPSMeanEstimate(Period=FY1, Curn=USD, RollPeriods=False)'])
 
 dfFY2, err = ek.get_data("IBM", 
-                 ['TR.RevenueMeanEstimate(Period=FY2, Scale=6, Curn=USD)',
-                  'TR.RevenueHigh(Period=FY2, Scale=6, Curn=USD)', 'TR.RevenueLow(Period=FY2, Scale=6, Curn=USD)',
-                  'TR.EBITDAMean(Period=FY2, Scale=6, Curn=USD, Precision=2)',
-                  'TR.EBITDAHigh(Period=FY2, Scale=6, Curn=USD)', 'TR.EBITDALow(Period=FY2, Scale=6, Curn=USD)',
+                 ['TR.RevenueMeanEstimate(Period=FY2, Scale=6, Curn=USD, Precision=0)',
+                  'TR.RevenueHigh(Period=FY2, Scale=6, Curn=USD, Precision=0)',
+                  'TR.RevenueLow(Period=FY2, Scale=6, Curn=USD, Precision=0)',
+                  'TR.EBITDAMean(Period=FY2, Scale=6, Curn=USD, Precision=0)',
+                  'TR.EBITDAHigh(Period=FY2, Scale=6, Curn=USD, Precision=0)',
+                  'TR.EBITDALow(Period=FY2, Scale=6, Curn=USD, Precision=0)',
                   'TR.EPSMeanEstimate(Period=FY2, Curn=USD)', 'TR.EPSHigh(Period=FY2, Curn=USD)', 'TR.EPSLow(Period=FY2, Curn=USD)'])
 
 dfFY2Roll, err = ek.get_data("IBM", 
-                     ['TR.RevenueMeanEstimate(Period=FY2, Scale=6, Curn=USD, RollPeriods=False)',
-                      'TR.EBITDAMean(Period=FY2, Scale=6, Curn=USD, RollPeriods=False)',
-                      'TR.EPSMeanEstimate(Period=FY2, Curn=USD, RollPeriods=False, Precision=2)'])
+                     ['TR.RevenueMeanEstimate(Period=FY2, Scale=6, Curn=USD, RollPeriods=False, Precision=0)',
+                      'TR.EBITDAMean(Period=FY2, Scale=6, Curn=USD, RollPeriods=False, Precision=0)',
+                      'TR.EPSMeanEstimate(Period=FY2, Curn=USD, RollPeriods=False)'])
 
 dfFY0
 dfFY1
@@ -45,13 +49,13 @@ print("\n\nIBM\tSales (MIL)\t\t\t\t\tEst. 30\t\tEBITDA (MIL)\t\t\t\t\tEst.30\t\t
 "FY0\t" + str(dfFY0['Revenue - Actual'][0])+"\t\t\t\t\t\t\t\t"+str(dfFY0['EBITDA - Actual'][0])+
 "\t\t\t\t\t\t\t\t"+str(dfFY0['Earnings Per Share - Actual'][0])+"\n"+
 
-"FY1\t" + str(dfFY1['Revenue - Mean Estimate'][0])+"\t"+ str(dfFY1['Revenue - High'][0])+"\t\t"+str(dfFY1['Revenue - Low'][0])+"\t\t"+
-str(dfFY1Roll['Revenue - Mean Estimate'][0])+"\t"+str(dfFY1['EBITDA - Mean'][0])+"\t"+str(dfFY1['EBITDA - High'][0])+"\t\t"+
-str(dfFY1['EBITDA - Low'][0])+"\t"+str(dfFY1Roll['EBITDA - Mean'][0])+"\t"+
+"FY1\t" + str(dfFY1['Revenue - Mean Estimate'][0])+"\t\t"+ str(dfFY1['Revenue - High'][0])+"\t\t"+str(dfFY1['Revenue - Low'][0])+"\t\t"+
+str(dfFY1Roll['Revenue - Mean Estimate'][0])+"\t\t"+str(dfFY1['EBITDA - Mean'][0])+"\t\t"+str(dfFY1['EBITDA - High'][0])+"\t\t"+
+str(dfFY1['EBITDA - Low'][0])+"\t\t"+str(dfFY1Roll['EBITDA - Mean'][0])+"\t\t"+
 str(dfFY1['Earnings Per Share - Mean Estimate'][0])+"\n"+
 
-"FY2\t" + str(dfFY2['Revenue - Mean Estimate'][0])+"\t"+ str(dfFY2['Revenue - High'][0])+"\t\t"+str(dfFY2['Revenue - Low'][0])+"\t\t"+
-str(dfFY2Roll['Revenue - Mean Estimate'][0])+"\t"+str(dfFY2['EBITDA - Mean'][0]) +"\t"+str(dfFY2['EBITDA - High'][0])+"\t"+
-str(dfFY2['EBITDA - Low'][0])+"\t"+str(dfFY2Roll['EBITDA - Mean'][0])+"\t"+
+"FY2\t" + str(dfFY2['Revenue - Mean Estimate'][0])+"\t\t"+ str(dfFY2['Revenue - High'][0])+"\t\t"+str(dfFY2['Revenue - Low'][0])+"\t\t"+
+str(dfFY2Roll['Revenue - Mean Estimate'][0])+"\t\t"+str(dfFY2['EBITDA - Mean'][0]) +"\t\t"+str(dfFY2['EBITDA - High'][0])+"\t\t"+
+str(dfFY2['EBITDA - Low'][0])+"\t\t"+str(dfFY2Roll['EBITDA - Mean'][0])+"\t\t"+
 str(dfFY2['Earnings Per Share - Mean Estimate'][0]))
 
